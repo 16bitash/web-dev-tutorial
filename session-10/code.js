022b67d54d60aee3c7a14a64e5ed071026a54d0f
Ashish Kumar <kashish@tekion.com>
2026-05-16 17:08:54 +0530

    add session 10

---
+++ session-10/code.js
@@ -0,0 +1,77 @@
+// function isEven(num) {
+//   const remainder = num % 2;
+  
+//   if (remainder === 0) {
+//     return true;
+//   }
+
+//   if (remainder === 1) {
+//     return false;
+//   }
+// }
+
+// const result = isEven(7);
+// console.log(result);
+
+
+// // 0 - 32 => F
+// // 33 - 44 => E
+// // 45 - 49 => D
+// // 50 - 59 => C
+// // 60 - 69 => B
+// // 70 - 100 => A
+// function gradeCalculator(marks) {
+//   if (marks <= 32) {
+//     return 'F';
+//   } else if (marks <= 44) {
+//     return 'E';
+//   } else if (marks <= 49) {
+//     return 'D';
+//   } else if (marks <= 59) {
+//     return 'C';
+//   } else if (marks <= 69) {
+//     return 'B';
+//   } else {
+//     return 'A';
+//   }
+// };
+
+// const grade = gradeCalculator(70);
+// console.log(grade);
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// let iteration = 1;
+// while (iteration <= 10) {
+//   console.log(iteration);
+//   iteration = iteration + 1;
+// }
+
+// for (let iteration = 1; iteration <= 10; iteration = iteration + 1) {
+//   console.log(iteration);
+// }
+
+// function isBetween50and100(num) {
+//   if (num >= 50 && num <= 100)  {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function isCatOrDogOrCamel(animal) {
+//   if (animal === 'dog' || animal === 'cat' || animal === 'camel') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
